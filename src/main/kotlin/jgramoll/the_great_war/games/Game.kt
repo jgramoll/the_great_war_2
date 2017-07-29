d72643f053120075953372de3d4a5fd3058889bd
jgramoll <jgramoll@instructure.com>
2017-07-28 20:59:08 -0600

    fix yarn build for ci

--- src/main/kotlin/jgramoll/the_great_war/games/Game.kt
+++ src/main/kotlin/jgramoll/the_great_war/games/Game.kt
@@ -9,13 +9,4 @@ import javax.persistence.Id
 data class Game(
         val title: String,
         @Id @GeneratedValue(strategy = GenerationType.AUTO)
-        val id: Long = -1) {
-
-    override fun toString(): String {
-        return "Game(id=$id, title='$title')"
-    }
-
-    // TODO get these to work with code coverage
-    override operator fun equals(other: Any?) = super.equals(other)
-    override fun hashCode() = super.hashCode()
-}
+        val id: Long = -1)
